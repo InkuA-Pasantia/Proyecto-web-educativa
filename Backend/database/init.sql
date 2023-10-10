ef201c7263eeb7f14e70d00d9a4944cefdb1c972
Noelia-Ruiz <ruiz.noeliaromina@gmail.com>
2023-10-10 01:09:41 -0300

    Archivo para la creación de la bd en pgAdmin

---
+++ Backend/database/init.sql
@@ -0,0 +1,25 @@
+CREATE TABLE cursos (
+    id SERIAL PRIMARY KEY, --la llave primaria
+    nombreCurso VARCHAR(255) UNIQUE NOT NULL,
+    lenguaje TEXT NOT NULL,
+    duracion VARCHAR(255),
+    estrellas INT,
+    dificultad TEXT,
+    descripcion VARCHAR(500),
+    contenido TEXT
+);
+
+CREATE TABLE usuarios(
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL,
+    apellido VARCHAR(255) NOT NULL,
+    edad INT,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    pass VARCHAR(255) NOT NULL,
+    cellphone INT,
+    birthday DATE,
+    rol TEXT,
+    fecha_registro TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    fecha_actualizacion TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
