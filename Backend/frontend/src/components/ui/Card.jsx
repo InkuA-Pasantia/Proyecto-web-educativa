d8bffa5ebc57b446a12ae2b469ff1bce650d1e3f
Noelia-Ruiz <ruiz.noeliaromina@gmail.com>
2023-10-21 02:57:34 -0300

    Formulario de registro parte 2

--- Backend/frontend/src/components/ui/Card.jsx
+++ Backend/frontend/src/components/ui/Card.jsx
@@ -1,5 +1,6 @@
 export function Card({ children }) {
-  return <div className="bg-zinc-900 p-14 rounded-md">{children}</div>;
+  return (
+    <div className="bg-zinc-900 p-14 rounded-md">{children}</div>)
 }
 
 export default Card;
--- Backend/frontend/src/pages/RegisterPage.jsx
+++ Backend/frontend/src/pages/RegisterPage.jsx
@@ -1,16 +1,42 @@
-import { Input } from "../components/ui/Input";
-import { Card } from "../components/ui/Card";
-import { Button } from "../components/ui/Button";
+import { Button, Card, Input } from "../components/ui"; //importamos todo en una línea
+import { useForm } from 'react-hook-form';
 
 function RegisterPage() {
+
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+  });
+
   return (
     <div className="h-[calc(100vh-64px)] flex items-center justify-center">
       <Card>
         <h3 className="text-2xl font-bold">Registro</h3>
-        <form>
-          <Input placeholder="Ingrese su nombre: "></Input>
-          <Input type="email" placeholder="Ingrese su email: "></Input>
-          <Input type="password" placeholder="Ingrese su contraseña: "></Input>
+        <form onSubmit={onSubmit}>
+
+          <Input placeholder="Ingrese su nombre: "
+            {...register("name", { required: true })}></Input>
+
+          {
+            errors.name && <span className="text-red-500">Este campo es requerido</span>
+
+          }
+
+          <Input type="email" placeholder="Ingrese su email: "
+            {...register("email", { required: true })}></Input>
+
+          {
+            errors.email && <span className="text-red-500">Este campo es requerido</span>
+
+          }
+
+          <Input type="password" placeholder="Ingrese su contraseña: "
+            {...register("password", { required: true })}></Input>
+          {
+            errors.password && <span className="text-red-500">Este campo es requerido</span>
+
+          }
           <Button>Registrarse</Button>
         </form>
       </Card>
