a3ecfc6713b5700e8759bed0e13c6fd36c72063f
Gonzalo Quiroga <gonzaquirogasassu@gmail.com>
2023-10-21 00:32:51 -0300

    Formulario de registro Parte 1

--- Backend/frontend/src/pages/RegisterPage.jsx
+++ Backend/frontend/src/pages/RegisterPage.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { Input } from "../components/ui/Input";
+import { Card } from "../components/ui/Card";
+import { Button } from "../components/ui/Button";
 
 function RegisterPage() {
   return (
-    <h3 calssName='text-2xl font-bold text-yellow-300'>Registro</h3>
-  )
+    <div className="h-[calc(100vh-64px)] flex items-center justify-center">
+      <Card>
+        <h3 className="text-2xl font-bold">Registro</h3>
+        <form>
+          <Input placeholder="Ingrese su nombre: "></Input>
+          <Input type="email" placeholder="Ingrese su email: "></Input>
+          <Input type="password" placeholder="Ingrese su contraseña: "></Input>
+          <Button>Registrarse</Button>
+        </form>
+      </Card>
+    </div>
+  );
 }
-
-export default RegisterPage
+export default RegisterPage;
